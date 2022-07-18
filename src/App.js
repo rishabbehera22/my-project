553da4145d8339e1effebf3765a9351540515833
rishabbehera22 <105111090+rishabbehera22@users.noreply.github.com>
2022-07-18 21:17:50 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
-
+import Heading from "./Heading";
+import Home from "./Home"
+import Getthesolution from "./Getthesolution";
+import Counting from "./Counting";
+import Signinpage from "./Signinpage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="solution" element={<Solution />} />
+        <Route path="projectp" element={<Projectpage />} />
+      </Routes>
     </div>
   );
 }
+function Homepage() {
+  return (
+    <Home></Home>
+  );
+}
+function Solution() {
+  return (
+    <div>
+      <Heading></Heading>
+      <br />
+      <Counting></Counting>
+      <hr />
+      <hr />
 
+      <br />
+      <br />
+      <Getthesolution></Getthesolution>
+    </div>
+
+
+
+
+
+  );
+}
+function Projectpage() {
+  return (
+    <div>
+      <Heading></Heading>
+      <br />
+
+      <Signinpage></Signinpage>
+    </div>
+
+  )
+}
 export default App;
